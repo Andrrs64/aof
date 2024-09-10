fdfebe0d1738330e084e64fb37ff8ff3fae642e0
Mathias Retterås <mathias.retteras@ecit.no>
2024-09-10 10:53:11 +0200

    Day 3, Engine Schematic

---
+++ AdventOfCode/src/day3/day3.ts
@@ -0,0 +1,81 @@
+import {input} from './input'
+
+interface numberObject {
+    num: string;
+    line: number;
+    start: number;
+    end: number;
+}
+
+const isSymbol = (char:string) => {
+
+}
+
+export const day3 = () => {
+    const lines:string[] = []
+    input.split(/\r?\n|\r|\n/g).forEach((line:string) => {
+        if (!line.length) {
+            return
+        }
+        lines.push(line);
+    });
+    const numbers:numberObject[] = []
+    lines.forEach((current, index) => {
+        let prev: numberObject | undefined = undefined
+        for (let i =0; i < current.length; i++) {
+            const char = current.charAt(i);
+            if (/^[0-9]*$/.test (char)) {
+                if (prev !== undefined) {
+                    prev.end = i
+                    prev.num += char
+                }       
+                else {
+                    prev = {num: char, line: index, start: i, end: i}
+                }
+            }
+            else if (prev !== undefined) {
+                numbers.push(prev);
+                prev = undefined;
+            }
+        }
+        return 
+    })
+    let result = 0
+
+    console.log(numbers)
+    const num = numbers[9]
+    numbers.forEach((num) => {
+        const chars:string[] = []
+        if (num.start > 0) {
+            chars.push(lines[num.line].charAt(num.start - 1))
+        }
+        if (num.end < lines[num.line].length - 1) {
+            chars.push(lines[num.line].charAt(num.end + 1))
+        }
+        if (num.line > 0) {
+            let start = num.start > 0 ? num.start - 1 : num.start
+            let end = num.end < lines[num.line].length - 1 ? num.end + 1 : num.end
+            for (let i = start; i <= end; i++) {
+                chars.push(lines[num.line - 1].charAt(i))
+            }
+        }
+        if (num.line < lines.length - 1) {
+            let start = num.start > 0 ? num.start - 1 : num.start
+            let end = num.end < lines[num.line].length - 1 ? num.end + 1 : num.end
+            for (let i = start; i <= end; i++) {
+                chars.push(lines[num.line + 1].charAt(i))
+            }
+        }
+
+        let isSerialNumber = false
+        for (let index in chars) {
+            if (/[^0-9]/g.test(chars [index]) && chars [index] != '.') {
+                isSerialNumber = true
+            }
+        }
+        if (isSerialNumber) {
+            result += parseInt(num.num)
+        }
+    })
+    console.log(result)
+}
--- AdventOfCode/src/index.ts
+++ AdventOfCode/src/index.ts
@@ -1,7 +1,9 @@
 import {day1} from './day1/day1'
 import { day2 } from './day2/day2'
 import { day2part2 } from './day2/day2part2';
+import { day3 } from './day3/day3';
 
 //day1();
-day2();
-day2part2();
+//day2();
+//day2part2();
+day3();

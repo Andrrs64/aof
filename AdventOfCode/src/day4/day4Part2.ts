b85680909712f71d4a68795ff4b144c24894f1bb
Anders T <anders.thorsrud@rubic.no>
2024-09-12 14:29:33 +0200

    Day4Part2: Finished

--- AdventOfCode/src/day4/day4Part2.ts
+++ AdventOfCode/src/day4/day4Part2.ts
@@ -10,38 +10,38 @@ export const day4Part2 = () => {
         }
     });
     const cardNums: string[][] = []
-    const cardInstances: number[] = new Array<number>(lines.length).fill(0)
+    const cardInstances: number[] = new Array<number>(lines.length).fill(1)
     lines.forEach((line, lineNum) => {
-        cardInstances [lineNum] += 1
+        console.log('Card ' + (lineNum + 1))
         const winningNums: string[] = [];
 
-        for (let i = 0; i < 5; i++){
-            const start = 7 + (i * 3)
+        for (let i = 0; i < 10; i++){
+            const start = 9 + (i * 3)
             const num = line.slice(start, start + 3)
             winningNums.push(num.trim());
         }
-        console.log(winningNums)
+
         const ourNums: string = line.split('|')[1];
         cardNums.push([])
         
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < 25; i++) {
             const start = i * 3;
             const num = ourNums.slice(start, start + 3).trim()
             cardNums[lineNum].push(num)
         }
-        for (let index = 0; index <= lineNum; index++) {
-            let matchingNums = 0
-            winningNums.forEach((num) => {
-                if (cardNums[index].find((n) => n === num)) {
-                    matchingNums ++
-                }
-            })
-            for (let i = 1; i <= matchingNums; i++) {
-                cardInstances[lineNum + i] += cardInstances [index]
+
+        let matches = 0;
+        winningNums.forEach((num) => {
+            if (cardNums[lineNum].find((n) => n === num)) {
+                matches++
             }
+        })
+
+        for (let i = 1; i <= matches; i++) {
+            cardInstances[lineNum + i] += cardInstances[lineNum]
         }
     })
-    console.log(cardInstances);
+
     let result = 0
     cardInstances.forEach((instances) => {
         result += instances
